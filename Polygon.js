2ce302f2688bc6d39e15352b5e4de9779ec85461
PRATHAMESHVCHAUDHARI <84676564+PRATHAMESHVCHAUDHARI@users.noreply.github.com>
2021-08-13 15:57:48 +0530

    Update Polygon.js

--- Polygon.js
+++ Polygon.js
@@ -1,24 +1,31 @@
-class Polygon {
-    constructor(x, y, radius) {
-        var options = {
-            isStatic: false,
-            restitution: 0.5,
-            density: 0.2
+class Hex {
+
+    constructor(x,y,width,height) {
+
+        var options={
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0,
+            'isStatic':true
         }
-        this.body = Bodies.circle(x, y, radius, options);
-        this.radius = radius;
-        World.add(world, this.body);
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("hexagon (1).png");
+        World.add(world,this.body);
+
     }
-    display(color, border) {
-        var pos = this.body.position;
-        var angle = this.angle;
+
+    display() {
+
+        var angle = this.body.angle;
         push();
-        ellipseMode(RADIUS);
-        translate(pos.x, pos.y);
-        fill(color);
-        stroke(border);
-        strokeWeight(4);
-        ellipse(0,0,this.radius,this.radius);
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image ,this.body.position.x,this.body.position.y,40,40);
         pop();
+
     }
 }
